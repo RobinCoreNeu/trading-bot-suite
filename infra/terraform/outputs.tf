2b836e5de66507be52f16ec07309cee6cd15ae82
RobinCoreNeu <stevepergelt@googlemail.com>
2025-10-28 17:28:45 +0100

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "server_ip" {
-  description = "The floating IP of the server"
-  value       = hcloud_floating_ip.robin_core.ip_address
+  value = hcloud_floating_ip.robin_core.ip_address
 }
 
 output "server_id" {
-  description = "The ID of the server"
-  value       = hcloud_server.robin_core.id
+  value = hcloud_server.robin_core.id
 }
