64ba8c4380cf349e52716c08a2c9819693e3770d
RobinCoreNeu <stevepergelt@googlemail.com>
2025-10-28 17:09:51 +0100

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -19,64 +19,64 @@ resource "hcloud_ssh_key" "default" {
 
 resource "hcloud_firewall" "robin_core" {
   name = "robin-core-firewall"
-  
+
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "22"
     source_ips = [
       "0.0.0.0/0",
-      "::/0"
+      "::/0",
     ]
   }
-  
+
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "80"
     source_ips = [
       "0.0.0.0/0",
-      "::/0"
+      "::/0",
     ]
   }
-  
+
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "443"
     source_ips = [
       "0.0.0.0/0",
-      "::/0"
+      "::/0",
     ]
   }
-  
+
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "3000"
     source_ips = [
       "0.0.0.0/0",
-      "::/0"
+      "::/0",
     ]
   }
-  
+
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "9090"
     source_ips = [
       "0.0.0.0/0",
-      "::/0"
+      "::/0",
     ]
   }
-  
+
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "9100"
     source_ips = [
       "0.0.0.0/0",
-      "::/0"
+      "::/0",
     ]
   }
 }
@@ -86,12 +86,12 @@ resource "hcloud_server" "robin_core" {
   image       = "ubuntu-24.04"
   server_type = "cx21"
   location    = "fsn1"
-  
-  ssh_keys = [hcloud_ssh_key.default.id]
+
+  ssh_keys    = [hcloud_ssh_key.default.id]
   firewall_ids = [hcloud_firewall.robin_core.id]
-  
+
   user_data = file("${path.module}/cloud-init.yml")
-  
+
   lifecycle {
     ignore_changes = [user_data]
   }
